1b0411e20af0ba92642803318c08000d3d242b97
Simon Enenche <enenchesimon@gmail.com>
2025-06-12 11:52:16 -0400

    Don't fuck this up

--- frontend/src/API.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: "/api",
-    headers: {
-        'Accept':'application/json',
-        'Content-Type':'application/json',
-    }
-})
